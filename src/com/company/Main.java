b13eb65aec6907192b08e53bea8d2034436bb49b
rafsun-dev <rafsun.dev@gmail.com>
2021-03-29 14:11:27 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int number1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int number2 = input.nextInt();
+        int large = (number1 > number2) ? number1 : number2;
+        System.out.println("The large number is: "+large);
+    }
+}
